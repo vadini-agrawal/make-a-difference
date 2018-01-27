611c46ef029b3fb1c0bbf65a03273f965f5e766f
dds1999 <dds1999@users.noreply.github.com>
2018-01-27 18:34:40 -0500

    working on sign up

--- script.js
+++ script.js
@@ -9,21 +9,6 @@
   };
   firebase.initializeApp(config);
 
-
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
-
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
-
     function handleSignUp() {
       var email = document.getElementById('email').value;
       var password = document.getElementById('password').value;
