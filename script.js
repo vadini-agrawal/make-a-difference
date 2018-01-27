6016e9cc186bb8b288b8f27ea4e16f6d92c2342f
Amy Pollmann <amy.pollmann@gmail.com>
2018-01-27 17:50:18 -0500

    idk

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDxKqUB6_fZgmxLrnP6RclWpnXDPWY-t8M",
+    authDomain: "healthy-friends.firebaseapp.com",
+    databaseURL: "https://healthy-friends.firebaseio.com",
+    projectId: "healthy-friends",
+    storageBucket: "healthy-friends.appspot.com",
+    messagingSenderId: "259118374654"
+  };
+  firebase.initializeApp(config)
+
+
 function check(form)/*function to check userid & password*/
   {
   /*the following code checkes whether the entered userid and password are matching*/
-  if(form.userid.value == "myuserid" && form.pswrd.value == "mypswrd") {
-    window.open('target.html')/*opens the target page while Id & password matches*/
+  if(form.userid.value == "foo@email.com" && form.pswrd.value == "password") {
+    window.open('landingPage.html')/*opens the target page while Id & password matches*/
   } else {
     alert("Error Password or Username")/*displays error message*/
   }
