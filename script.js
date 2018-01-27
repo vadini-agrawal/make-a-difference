abc1edd390129b7224d30b7b52e241c954efb152
dds1999 <dds1999@users.noreply.github.com>
2018-01-27 18:14:11 -0500

    sign up button

--- script.js
+++ script.js
@@ -10,6 +10,47 @@
   firebase.initializeApp(config)
 
 
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+    function handleSignUp() {
+      var email = document.getElementById('email').value;
+      var password = document.getElementById('password').value;
+      // if (email.length < 4) {
+      //   alert('Please enter an email address.');
+      //   return;
+      // }
+      // if (password.length < 4) {
+      //   alert('Please enter a password.');
+      //   return;
+      // }
+      // Sign in with email and pass.
+      // [START createwithemail]
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+
 function check(form)/*function to check userid & password*/
   {
   /*the following code checkes whether the entered userid and password are matching*/
@@ -19,3 +60,5 @@ function check(form)/*function to check userid & password*/
     alert("Error Password or Username")/*displays error message*/
   }
 }
+
+
