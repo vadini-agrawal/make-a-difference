aef172197b3702872c48c305c553484e1a33648e
dds1999 <dds1999@users.noreply.github.com>
2018-01-27 18:23:27 -0500

    sign up button

--- script.js
+++ script.js
@@ -57,13 +57,12 @@ firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error
         });
     });
 
-function check(form)/*function to check userid & password*/
-  {
+function check(form)/*function to check userid & password*/ {
   /*the following code checkes whether the entered userid and password are matching*/
-  if(form.userid.value == "foo@email.com" && form.pswrd.value == "password") {
-    window.open('landingPage.html')/*opens the target page while Id & password matches*/
+  if(form.userid.value == "foo@email.com" && form.passwrd.value == "password") {
+    window.open('landingPage.html');/*opens the target page while Id & password matches*/
   } else {
-    alert("Error Password or Username")/*displays error message*/
+    alert("Error Password or Username")/*displays error message*/;
   }
 }
 
